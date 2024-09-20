5a6c9fda9c3fda45aceff7e09daede22c7cccfb4
Darek <34578369+darekbx@users.noreply.github.com>
2024-09-20 22:41:56 +0200

    Add weight view model

---
+++ Weight/Weight/WeightApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightApp.swift
+//  Weight
+//
+//  Created by Dariusz Baranczuk on 17/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Weight/Weight/WeightViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeightViewModek.swift
+//  Weight
+//
+//  Created by Dariusz Baranczuk on 17/09/2024.
+//
+
+import Foundation
